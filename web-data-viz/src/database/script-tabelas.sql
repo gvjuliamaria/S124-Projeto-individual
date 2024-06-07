2018b84a38ce34f2e4bd1c24a3095190c48e1713
gvjuliamaria <julia.almeida@sptech.school>
2024-06-06 23:57:56 -0300

    Atualização script-tabela

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,59 +1,25 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 
-/*
-comandos para mysql server
-*/
+-- DROP DATABASE chacaraGouveia;
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
-);
+CREATE DATABASE chacaraGouveia;
+USE chacaraGouveia;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+  idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(200),
+  email VARCHAR(100),
+  idade INT,
+  frequenciaContato VARCHAR(45),
+  nivelEstresse VARCHAR(45),
+  sentimento VARCHAR(45)
+  );
+  
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+CREATE TABLE simulacao (
+  idSimulacao INT AUTO_INCREMENT,
+  planta VARCHAR(45),
+  meses VARCHAR(45),
+  fkUsuario INT,
+  PRIMARY KEY (idSimulacao, fkUsuario),
+  CONSTRAINT fk_simulacao_usuario FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+  );
